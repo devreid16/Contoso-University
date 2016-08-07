3825e47945856aaebb6940a2d353727168f2cdf8
Angela Reid <reidang94@gmail.com>
2016-08-07 18:44:36 -0400

    correct class name

--- ContosoUniversity/Models/Students.cs
+++ ContosoUniversity/Models/Students.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace ContosoUniversity.Models
 {
-    public class Students
+    public class Student
     {
         public int ID { get; set; }
         public string LastName { get; set; }
