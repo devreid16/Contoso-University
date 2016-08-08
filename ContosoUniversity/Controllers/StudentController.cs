cbe3cd2ea35b6b076d36ec314a2acb3b61ed18e2
Angela Reid <reidang94@gmail.com>
2016-08-08 10:12:47 -0400

    replace controller, add editing

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -13,17 +13,16 @@ namespace ContosoUniversity.Controllers
 {
     public class StudentController : Controller
     {
-        //instantiates a database context object
         private SchoolContext db = new SchoolContext();
 
-        // GET: Students
+        // GET: Student
         public ActionResult Index()
         {
-            //get list of students from students entity
             return View(db.Students.ToList());
         }
 
-        // GET: Students/Details/5
+       
+        // GET: Student/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,14 +36,14 @@ namespace ContosoUniversity.Controllers
             }
             return View(student);
         }
-
-        // GET: Students/Create
+        
+        // GET: Student/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Students/Create
+        // POST: Student/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -60,7 +59,6 @@ namespace ContosoUniversity.Controllers
                     return RedirectToAction("Index");
                 }
             }
-
             catch (DataException /* dex */)
             {
                 //Log the error (uncomment dex variable name and add a line here to write a log.
@@ -69,22 +67,36 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
-        // GET: Students/Edit/5
+        // GET: Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public ActionResult EditPost(int? id)
         {
             if (id == null)
-            {                
+            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            //used to prevent over posting by using specific properties
             var studentToUpdate = db.Students.Find(id);
-
-            //tries to update user form input using "whitelisted" updateable fields
             if (TryUpdateModel(studentToUpdate, "",
-                    new string[] {"LastName", "FirstMidName", "EnrollmentDate" }))
+               new string[] { "LastName", "FirstMidName", "EnrollmentDate" }))
             {
                 try
                 {
@@ -92,32 +104,16 @@ namespace ContosoUniversity.Controllers
 
                     return RedirectToAction("Index");
                 }
-
-                catch (DataException /* dev */)
+                catch (DataException /* dex */)
                 {
-                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
                 }
             }
             return View(studentToUpdate);
         }
 
-        // POST: Students/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
-
-        // GET: Students/Delete/5
+        // GET: Student/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -132,7 +128,7 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
-        // POST: Students/Delete/5
+        // POST: Student/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -23,6 +23,7 @@ namespace ContosoUniversity.DAL
             new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
             new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
             new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+
             };
 
             //For each entity type, the code creates a collection of new
